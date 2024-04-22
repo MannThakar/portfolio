a7c7ac2422536c2416be18e8299e3d2c2ee3f40e
Titty <mehran9326@gmail.com>
2024-04-23 02:13:43 +0330

    add font, some styles and background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        Quicksand: ["Quicksand", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -76,6 +79,10 @@ module.exports = {
         hero_shape_1: "url(/hero/shape-1.svg)",
         hero_shape_2_light: "url(/hero/shape-2-light.svg)",
         hero_shape_2_dark: "url(/hero/shape-2-dark.svg)",
+        dots_light: "url(/dots-light.svg)",
+        dots_dark: "url(/dots-dark.svg)",
+        about_shape_light: "url(/about/shape-light.svg)",
+        about_shape_dark: "url(/about/shape-dark.svg)",
       },
     },
   },
